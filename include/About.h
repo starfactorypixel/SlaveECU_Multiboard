188be03f0c85e849f959ae22c0e6f2a592a74558
GrKoR <dr.g.kornev@gmail.com>
2024-06-01 00:14:43 +0300

    fix: board_ver++, board_v1 url

--- include/About.h
+++ include/About.h
@@ -5,7 +5,7 @@ namespace About
 	static constexpr char name[] = "MultiboardECU";
 	static constexpr char desc[] = "Multi board control board for Pixel project";
 	static constexpr char board_type = 0x10;		// 5 bits
-	static constexpr char board_ver = 1;		// 3 bits
+	static constexpr char board_ver = 2;		// 3 bits
 	static constexpr char soft_ver = 1;			// 6 bits
 	static constexpr char can_ver = 1;			// 2 bits
 	static constexpr char git[] = "https://github.com/starfactorypixel/SlaveECU_Multiboard";
